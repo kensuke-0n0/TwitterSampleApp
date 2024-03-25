72d9672944f8d09dee17c4ccc595cd4baa6bc9fc
kensuke-0n0 <kensuke.n9925@gmail.com>
2024-03-25 23:14:53 +0900

    UITableViewによるUI作成(デフォルトUITableViewCell)

---
+++ TwitterSampleApp/TimeLineViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TimeLineViewController.swift
+//  TwitterSampleApp
+//
+//  Created by にしいけんすけ on 2024/03/24.
+//
+
+import Foundation
+import UIKit
+
+class TimeLineViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+    }
+}
+
+extension TimeLineViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
---
+++ TwitterSampleApp/TwitterSampleApp.swift
@@ -0,0 +1,8 @@
+//
+//  TwitterSampleApp.swift
+//  TwitterSampleApp
+//
+//  Created by にしいけんすけ on 2024/03/24.
+//
+
+import Foundation
