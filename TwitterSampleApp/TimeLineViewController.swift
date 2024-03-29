827f959707dfb077aa02117b61c72a22acc239e8
kensuke-0n0 <kensuke.n9925@gmail.com>
2024-03-30 01:14:07 +0900

    configureTableView()の下の空白を削除しました

--- TwitterSampleApp/TimeLineViewController.swift
+++ TwitterSampleApp/TimeLineViewController.swift
@@ -14,7 +14,6 @@ class TimeLineViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-
     }
     
     func configureTableView() {
